9efb2f0a0828495e19729a14c0fe42162eec3978
Cristopher Moreno Palmer <cmorenop@my.centennialcollege.ca>
2022-06-16 04:45:56 -0400

    initial commit

---
+++ config/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCwJQYHftk5vs0sMuLF9i0Ok83f31WYJKc",
+  authDomain: "mylms-945f2.firebaseapp.com",
+  projectId: "mylms-945f2",
+  storageBucket: "mylms-945f2.appspot.com",
+  messagingSenderId: "1063850657300",
+  appId: "1:1063850657300:web:e2891f04dfed0843ef4901"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
