2edcde14aa898ed17bb8819f83170129fc5eb164
Cristopher Moreno Palmer <cmorenop@my.centennialcollege.ca>
2022-06-18 03:11:10 -0400

    initial deploy

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // //! ROUTER  
 app.use('/courses', coursesRouter);
+app.use('/', coursesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
